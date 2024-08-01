c85954ebaa15096bbff0309fede19fbfabd25f13
thukralvinayak2003 <thukralvinayak2003@gmail.com>
2024-08-01 13:46:06 +0530

    Intial commit

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 dotenv.config({ path: ".env" });
 
 const corsOptions = {
-  origin: "https://to-do-frontend-kw4v.vercel.app/",
+  origin: "https://to-do-frontend-kw4v.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
